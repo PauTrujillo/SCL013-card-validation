ddc36c91f12d3ec11913de75e3efc2823d3c5f02
Carlareneedc <carla.renee.diazc@gmail.com>
2020-04-08 09:48:16 -0300

    este si

--- src/validator.js
+++ src/validator.js
@@ -1,2 +1,14 @@
+
+function isValid(creditCardNumber);
+let creditCardNumber =[];
+
+const validator= {
+
+};
+
 function isValid(creditCardNumber)
+
 export default validator;
+
+let creditCardNumber = document.getElementsByTagName("input")[0]; 
+alert(creditCardNumber);
