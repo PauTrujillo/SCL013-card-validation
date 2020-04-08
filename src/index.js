d258647d7ce8d1e5b63bd7afb06929aa38a1c1a3
PauTrujillo <pautrujillomorales@gmail.com>
2020-04-08 08:39:23 -0400

    aca van los cacambios revisados a la fecha

--- src/index.js
+++ src/index.js
@@ -5,8 +5,35 @@ console.log(validator);
 
 //BOTONES
 const botonParaFlow1= document.getElementById("boton1");
-
 botonParaFlow1.addEventListener("click", flow1);
+
+const botonParaForbidden= document.getElementById("boton2");
+botonParaForbidden.addEventListener("click", forbidden);
+
+const botonParaInformation= document.getElementById("boton3");
+botonParaInformation.addEventListener("click", information);
+
+const botonParaFlow2= document.getElementById("boton4");
+botonParaFlow2.addEventListener("click", flow2);
+
+const botonParaComeback1= document.getElementById("boton5");
+botonParaComeback1.addEventListener("click", cancel);
+
+const botonParaComeback2= document.getElementById("boton6");
+botonParaComeback2.addEventListener("click", cancel);
+
+const botonParaComeback3= document.getElementById("boton7");
+botonParaComeback3.addEventListener("click", cancel);
+
+const botonParaFlow3= document.getElementById("boton8");
+botonParaFlow3.addEventListener("click", flow3);
+
+const botonParaComeback4= document.getElementById("boton9");
+botonParaComeback4.addEventListener("click", cancel);
+
+const botonParaComeback5= document.getElementById("boton10");
+botonParaComeback5.addEventListener("click", cancel);
+
 
 //FUNCIONES PARA BOTONES
 function flow1(){
@@ -14,34 +41,35 @@ function flow1(){
     let intro=document.getElementById("welcome");
     flujo1.style.display= "block";
     intro.style.display= "none";
-  };
+  }
   
   function forbidden(){
     let accesoProhibido=document.getElementById("forbiddenAccess");
     let intro=document.getElementById("welcome");
     accesoProhibido.style.display= "block";
     intro.style.display = "none";
-  };
+  }
+
   function information(){
     let institucional= document.getElementById("about");
     let intro= document.getElementById("welcome");
     institucional.style.display= "block";
     intro.style.display = "none";
-  };
+  }
   
   function flow2(){
     let flujo1=document.getElementById("enterCard");
     let flujo2=document.getElementById("contactForm");
     flujo1.style.display="none";
     flujo2.style.display="block";
-  };
+  }
   
   function flow3(){
     let flujo2=document.getElementById("contactForm");
     let flujo3=document.getElementById("finalScreen");
     flujo2.style.display="none";
     flujo3.style.display="block";
-  };
+  }
   
   function cancel(){
     let cancelar=document.getElementById("welcome")
@@ -56,4 +84,7 @@ function flow1(){
     flujo3.style.display="none";
     accesoProhibido.style.display="none";
     institucional.style.display="none";
-  };
+  }
+
+
+  
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,10 @@
+
+
 const validator = {
-  // ...
+
 };
 
 export default validator;
+
+let creditCardNumber = document.getElementsByTagName("input")[0]; 
+alert(creditCardNumber);
